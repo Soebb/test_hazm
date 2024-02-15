27c10eb9b24bfa11419088ec8314821fce7c1806
Soebb <87156166+Soebb@users.noreply.github.com>
2024-02-15 17:42:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,10 +48,10 @@ async def start(bot, update):
 @Bot.on_message(filters.private & filters.text)
 async def t2s(bot, m):
     input = m.text
-    run_process = subprocess.run("./ezafeh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=input)
+    run_process = subprocess.run("./ezafeh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=input, encoding='ascii')
     error = run_process.stderr.decode()
     print(error)
-    out = run_process.stdin.decode()
+    out = run_process.stdout.decode()
     msg = await m.reply(out)
 
 
