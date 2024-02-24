4586252cb59f5b7f22913443cfe6d6e4cc57fe8d
Soebb <87156166+Soebb@users.noreply.github.com>
2024-02-24 15:33:39 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
-c = ["g++", "-Wall", "kasre_ezafeh.cpp", "hazm.cpp", "hazm.h", "-o", "ezafeh"]
-os.system('python3.10-config --ldflags --embed')
+c = ["g++", "-I/usr/include/python3.10" ,"-Wall", "kasre_ezafeh.cpp", "hazm.cpp", "hazm.h", "-o", "ezafeh"]
+#os.system('python3-config --ldflags --embed')
 compile_process = subprocess.run(c, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 if compile_process.returncode == 0: 
     print("Compilation successful.")
